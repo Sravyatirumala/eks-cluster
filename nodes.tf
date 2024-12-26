39edace2bbe177e3eea43891be2bd54a3beef204
RevanthReddyCh <crr.190295@gmail.com>
2024-12-26 14:11:25 +0530

    2nd subnet added

--- nodes.tf
+++ nodes.tf
@@ -3,7 +3,7 @@ resource "aws_launch_template" "eks_launch_configuration" {
   name          = "eks-launch-configuration"
   image_id      = "ami-0e2c8caa4b6378d8c" # Replace with the latest Amazon EKS optimized AMI
   instance_type = "t3.medium"
-  security_groups = [aws_security_group.allow_all_traffic.id]
+  vpc_security_group_ids = ["${aws_security_group.allow_all_traffic.id}"]
   iam_instance_profile = aws_iam_instance_profile.eks_node_instance_profile.id
 
   lifecycle {
@@ -17,7 +17,7 @@ resource "aws_autoscaling_group" "eks_node_group" {
   max_size             = 3
   min_size             = 1
   vpc_zone_identifier  = [aws_subnet.eks_subnet.id]
-  launch_configuration = aws_launch_configuration.eks_launch_configuration.id
+  launch_configuration = aws_launch_template.eks_launch_configuration.id
 }
 
 # IAM Instance Profile for worker nodes
