7c1fc56666e933367dd927aa47514a4f417b5ae8
RevanthReddyCh <crr.190295@gmail.com>
2024-12-26 13:30:29 +0530

    2nd subnet added

--- vpc.tf
+++ vpc.tf
@@ -17,7 +17,7 @@ resource "aws_subnet" "eks_subnet" {
   }
 }
 
-resource "aws_subnet" "eks_subnet" {
+resource "aws_subnet" "eks_subnet2" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
