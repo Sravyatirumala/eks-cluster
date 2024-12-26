f2def729824325ac927f4b54f136f5267b451468
RevanthReddyCh <crr.190295@gmail.com>
2024-12-26 13:29:48 +0530

    2nd subnet added

--- vpc.tf
+++ vpc.tf
@@ -16,3 +16,14 @@ resource "aws_subnet" "eks_subnet" {
     Name = "eks-subnet"
   }
 }
+
+resource "aws_subnet" "eks_subnet" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "eks-subnet-2"
+  }
+}
