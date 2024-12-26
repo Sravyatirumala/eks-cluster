e8582d2b4807e14b3afef688829f2658587b424d
RevanthReddyCh <crr.190295@gmail.com>
2024-12-26 15:02:20 +0530

    2nd subnet added

--- iam.tf
+++ iam.tf
@@ -1,3 +1,4 @@
+# IAM role for EKS Cluster
 resource "aws_iam_role" "eks_role" {
   name               = "eks-cluster-role"
   assume_role_policy = jsonencode({
@@ -19,13 +20,13 @@ resource "aws_iam_role" "eks_role" {
   }
 }
 
-# Attach EKS policies to IAM role
+# Attach EKS policies to IAM role for EKS Cluster
 resource "aws_iam_role_policy_attachment" "eks_role_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.eks_role.name
 }
 
-# IAM role for worker nodes
+# IAM role for EKS worker nodes
 resource "aws_iam_role" "eks_node_role" {
   name               = "eks-node-role"
   assume_role_policy = jsonencode({
@@ -47,7 +48,7 @@ resource "aws_iam_role" "eks_node_role" {
   }
 }
 
-# Attach EKS node policies to IAM role
+# Attach EKS node policies to IAM role for worker nodes
 resource "aws_iam_role_policy_attachment" "eks_node_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = aws_iam_role.eks_node_role.name
@@ -72,3 +73,23 @@ resource "aws_iam_role_policy_attachment" "eks_node_policy_attachment_5" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
   role       = aws_iam_role.eks_node_role.name
 }
+
+# IAM role policy attachment for the worker nodes for the correct access
+
+resource "kubectl_config_map" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapRoles = jsonencode([
+      {
+        rolearn  = "arn:aws:iam::<AWS_ACCOUNT_ID>:role/${aws_iam_role.eks_node_role.name}"
+        username = "eks-node"
+        groups   = ["system:masters"]
+      }
+    ])
+  }
+}
+
