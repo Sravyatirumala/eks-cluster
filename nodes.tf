32af8d36aef87c6fd80f3a4f766310071b5cf85b
RevanthReddyCh <crr.190295@gmail.com>
2025-01-04 18:21:02 +0530

    node count modified

--- nodes.tf
+++ nodes.tf
@@ -14,9 +14,9 @@ resource "aws_eks_node_group" "public-nodes" {
   instance_types = ["t3.medium"]
 
   scaling_config {
-    desired_size = 2
+    desired_size = 3
     max_size     = 4
-    min_size     = 2
+    min_size     = 3
   }
 
   update_config {
