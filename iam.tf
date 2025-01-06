15a9ad475c7867f048e5c7cd5b51857c4f0d6f52
RevanthReddyCh <crr.190295@gmail.com>
2025-01-06 14:47:09 +0530

    iam modified

--- iam.tf
+++ iam.tf
@@ -1,5 +1,4 @@
-# IAM role for eks
-
+# EKS Cluster Role
 resource "aws_iam_role" "eks_role" {
   name = "eks-cluster-demo"
   tags = {
@@ -24,15 +23,13 @@ resource "aws_iam_role" "eks_role" {
 POLICY
 }
 
-# eks policy attachment
-
+# EKS Cluster Policy Attachment
 resource "aws_iam_role_policy_attachment" "eks-AmazonEKSClusterPolicy" {
   role       = aws_iam_role.eks_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-# role for nodegroup
-
+# EKS Worker Node Role
 resource "aws_iam_role" "nodes" {
   name = "eks-node-group-nodes"
 
@@ -48,19 +45,54 @@ resource "aws_iam_role" "nodes" {
   })
 }
 
-# IAM policy attachment to nodegroup
-
+# Attach AmazonEKSWorkerNodePolicy to Node Role
 resource "aws_iam_role_policy_attachment" "nodes-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = aws_iam_role.nodes.name
 }
 
+# Attach AmazonEKS_CNI_Policy to Node Role
 resource "aws_iam_role_policy_attachment" "nodes-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = aws_iam_role.nodes.name
 }
 
+# Attach AmazonEC2ContainerRegistryReadOnly to Node Role
 resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.nodes.name
+}
+
+# Custom EBS Permissions for EKS Nodes
+resource "aws_iam_policy" "nodes-ebs-policy" {
+  name        = "EKSNodeEBSPolicy"
+  description = "EBS permissions for EKS node group"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:CreateVolume",
+          "ec2:AttachVolume",
+          "ec2:DescribeVolumes",
+          "ec2:DeleteVolume"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Attach the Custom EBS Policy to Node Role
+resource "aws_iam_role_policy_attachment" "nodes-ebs-policy-attachment" {
+  policy_arn = aws_iam_policy.nodes-ebs-policy.arn
+  role       = aws_iam_role.nodes.name
+}
+
+# Optional: Attach AmazonS3ReadOnlyAccess to allow access to S3 buckets for the nodes (if needed)
+resource "aws_iam_role_policy_attachment" "nodes-AmazonS3ReadOnlyAccess" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
   role       = aws_iam_role.nodes.name
 }
