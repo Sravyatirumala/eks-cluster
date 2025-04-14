185e6b9840081714bf2a3f43091d617babba37a4
Sravyatirumala <104963297+Sravyatirumala@users.noreply.github.com>
2025-04-14 14:54:41 +0530

    Update nodes.tf

--- nodes.tf
+++ nodes.tf
@@ -14,9 +14,9 @@ resource "aws_eks_node_group" "public-nodes" {
   instance_types = ["t3.medium"]
 
   scaling_config {
-    desired_size = 3
-    max_size     = 4
-    min_size     = 3
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
   }
 
   update_config {
